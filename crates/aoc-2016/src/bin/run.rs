ea9777acf89f98428b7c83522c8f622e6e749454
Carlitos <zyrtwo@gmail>
2026-07-23 20:09:05 -0700

    added scaffolding for aoc

---
+++ crates/aoc-2016/src/bin/run.rs
@@ -0,0 +1,5 @@
+use aoc_2016::Year2016;
+
+fn main() -> anyhow::Result<()> {
+    aoc_core::runner::run::<Year2016>()
+}
---
+++ crates/aoc-2016/src/days/day01.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+
+pub fn part1(input: &str) -> Result<String> {
+    let _ = input;
+    Ok("TODO".to_string())
+}
+
+pub fn part2(input: &str) -> Result<String> {
+    let _ = input;
+    Ok("TODO".to_string())
+}
+
+#[cfg(test)]
+mod tests_part1 {
+    use super::*;
+
+    #[test]
+    fn placeholder_part1() -> Result<()> {
+        assert_eq!(part1("TODO")?, "TODO");
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests_part2 {
+    use super::*;
+
+    #[test]
+    fn placeholder_part2() -> Result<()> {
+        assert_eq!(part2("TODO")?, "TODO");
+        Ok(())
+    }
+}
